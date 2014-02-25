c6ebf8749a6d68a9efddef77c25389ae75d397b3
Kirk Wight <kwight@kwight.ca>
2014-02-24 17:55:11 -0800

    Eventbrite: Modify index.php and sidebar.php to coincide with Eventbrite Single Event.

--- index.php
+++ index.php
@@ -1,5 +1,24 @@
 <?php
- /**
- * @package eventbrite-parent
+/**
+ * Default template
+ *
+ * @package eventbrite-event
  */
+
+get_header();
 ?>
+			<div class="row">
+				<div class="span8">
+					<div class="left-col event-home-loop" id="content">
+						<?php get_template_part( 'tmpl/home-feature' ); ?>
+						<h1 class="pagetitle"><?php _e( 'Latest Event Updates', 'eventbrite-event' ); ?></h1>
+						<?php while( have_posts() ) : the_post(); ?>
+							<?php get_template_part( 'tmpl/post-loop' ); ?>
+						<?php endwhile; ?>
+						<?php eb_paging_nav(); ?>
+					</div>
+				</div>
+				<?php get_sidebar(); ?>
+			</div>
+<?php
+get_footer();
--- sidebar.php
+++ sidebar.php
@@ -2,11 +2,44 @@
 /**
  * Template for sidebar
  *
- * @package eventbrite-parent
+ * @package eventbrite-single
  */
 ?>
+
+<?php
+if ( eb_get_page_id( 'event-info' ) == get_queried_object_id() ) {
+	$events     = eb_api_get_featured_events();
+	$event      = array_shift( $events );
+	$event      = is_null( $event ) ? false : $event->event;
+	$venue_info = eb_get_venue_address( $event );
+	$map_url    = eb_get_venue_google_map_url( $event );
+
+	$venue_info = eb_get_venue_address( $event );
+	if ( isset( $venue_info['mailing-address'] ) )
+		$address = $venue_info['mailing-address'];
+}
+?>
+
 <aside class="span4" role="complementary">
 	<div class="sidebar">
+		<?php if ( ! empty( $map_url ) || ! empty( $address ) ) : ?>
+			<div class="event-location widget">
+				<h2 class="widget-title"><?php _e( 'Location', 'eventbrite-multi' ); ?></h2>
+
+				<?php if ( $map_url ) : ?>
+					<img class="event-map" src="<?php echo esc_url( $map_url ); ?>" />
+				<?php endif; ?>
+
+				<?php if ( ! empty( $address ) ) : ?>
+					<p class="venue-address">
+					<?php foreach ( $address as $key => $line ) : ?>
+						<span class="<?php echo esc_attr( $key ); ?>"><?php echo esc_html( $line ); ?></span><br/>
+					<?php endforeach; ?>
+					</p>
+				<?php endif; ?>
+			</div>
+		<?php endif; ?>
+
 		<?php dynamic_sidebar( 'sidebar-1' ); ?>
 	</div>
 </aside>
